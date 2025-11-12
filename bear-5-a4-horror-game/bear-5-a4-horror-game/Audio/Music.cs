9c4c7f7e0e0f823e027039ce4349d00e34cf8335
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2025-11-12 13:28:53 -0500

    Rename Camera

--- bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
+++ bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
@@ -11,7 +11,15 @@ namespace MohawkGame2D
     public class OST
     {
         // Place your variables here:
-        public void Audio()
+        Music BGM1 = Audio.LoadMusic("");
+        Music BGM2 = Audio.LoadMusic("");
+        Music BGM3 = Audio.LoadMusic("");
+        Music BGM4 = Audio.LoadMusic("");
+        Sound sfx1 = Audio.LoadSound("");
+        Sound sfx2 = Audio.LoadSound("");
+        Sound sfx3 = Audio.LoadSound("");
+        Sound sfx4 = Audio.LoadSound("");
+        public void Backgroundmusic()
         {
 
         }
