be4ad518126bcbfd1ba332893652690e31b7342c
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2025-11-18 11:02:53 -0500

    Add Death Screen and Death Toggle

---
+++ bear-5-a4-horror-game/bear-5-a4-horror-game/Senator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MohawkGame2D;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class Senator
+    {
+        Direction Camera = new Direction();
+        int cameraPosition;
+        int senatorscreen=3;
+        Vector2 senatorPosition;
+        bool isPlayerAlive = true;
+        public void DrawSenator()
+        {
+            // Player can only switch camera while alive
+            if (isPlayerAlive == true)
+            {
+                Camera.CameraPosition();
+            }
+            // Get Player Position
+             cameraPosition = Camera.ShareScreenPosition();
+            // Draw Senator if the Player can see them
+            if (cameraPosition == senatorscreen)
+            {
+                Draw.Rectangle(new Vector2(200, 200), new Vector2(20));
+            }
+        }
+        public bool HasNotKilledPlayer()
+        {
+            if (isPlayerAlive == false)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void DeathToggle()
+        {
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            {
+                isPlayerAlive = !isPlayerAlive;
+            }
+        }
+    }
+}
