0a8e6cfbf270ebff950b086ece437a5903bd33e0
Connor-DewDrop-87 <000971900@mohawkcollege.ca>
2025-11-21 10:01:35 -0500

    Fix Audio Files

--- bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
+++ bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
@@ -12,19 +12,19 @@ namespace MohawkGame2D
     {
 
         // Place your variables here:
-        Music FF01 = Audio.LoadMusic("../../../../../Audio/(Audio/First_Fantasy_OST 0.1.wav"); 
-        Music Cave = Audio.LoadMusic("../../../../../Audio/(Audio/Cave.wav"); 
-        Music Chill = Audio.LoadMusic("../../../../../Audio/(Audio/Chill.wav"); 
-        Music Credits = Audio.LoadMusic("../../../../../Audio/(Audio/credits music.wav"); 
-        Music Redsun = Audio.LoadMusic("../../../../../Audio/(Audio//Armstrong/Redsun.wav"); 
-        Music Idiot = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Senator Armstrong - Idiot.wav"); 
-        Music NanoMachines = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/_Senator - Nanomachines, Son.wav"); 
-        Music SlimJim = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Voicy_Snap into a slim jim.wav"); 
-        Music ComeOn = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Voicy_Senator Armstrong - Come on!.wav"); 
-        Music Source = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Voicy_My source.wav"); 
-        Music Omelette = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Making the mother of all omelettes here Jack.wav"); 
-        Music PipeSound = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Metal_Pipe_Impace.wav"); 
-        Music VineBoom = Audio.LoadMusic("../../../../../Audio/(Audio/vine-boom.wav"); 
+        Music FF01 = Audio.LoadMusic("../../../../../Audio/First_Fantasy_OST 0.1.wav"); 
+        Music Cave = Audio.LoadMusic("../../../../../Audio/Cave.wav"); 
+        Music Chill = Audio.LoadMusic("../../../../../Audio/Chill.wav"); 
+        Music Credits = Audio.LoadMusic("../../../../../Audio/credits music.wav"); 
+        Music Redsun = Audio.LoadMusic("../../../../../Audio/Armstrong/Redsun.wav"); 
+        Music Idiot = Audio.LoadMusic("../../../../../Audio/Armstrong/Senator Armstrong - Idiot.wav"); 
+        Music NanoMachines = Audio.LoadMusic("../../../../../Audio/Armstrong/_Senator - Nanomachines, Son.wav"); 
+        Music SlimJim = Audio.LoadMusic("../../../../../Audio/Armstrong/Voicy_Snap into a slim jim.wav"); 
+        Music ComeOn = Audio.LoadMusic("../../../../../Audio/Armstrong/Voicy_Senator Armstrong - Come on!.wav"); 
+        Music Source = Audio.LoadMusic("../../../../../Audio/Armstrong/Voicy_My source.wav"); 
+        Music Omelette = Audio.LoadMusic("../../../../../Audio/Armstrong/Making the mother of all omelettes here Jack.wav"); 
+        Music PipeSound = Audio.LoadMusic("../../../../../Audio/Armstrong/Metal_Pipe_Impace.wav"); 
+        Music VineBoom = Audio.LoadMusic("../../../../../Audio/vine-boom.wav"); 
       
      
         public void BackgroundMusic()
