8c9043f0a7ed56720ac4920279fa8e1cbd1652a8
KeatonBSpeers <000952262@mohawkcollege.CA>
2025-11-20 21:49:06 -0500

    Changed capilization to make it easier to use sounds effects

--- bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
+++ bear-5-a4-horror-game/bear-5-a4-horror-game/Audio/Music.cs
@@ -13,10 +13,10 @@ namespace MohawkGame2D
 
         // Place your variables here:
         Music FF01 = Audio.LoadMusic("../../../../../Audio/(Audio/First_Fantasy_OST 0.1.wav"); 
-        Music CAVE = Audio.LoadMusic("../../../../../Audio/(Audio/Cave.wav"); 
-        Music CHILL = Audio.LoadMusic("../../../../../Audio/(Audio/Chill.wav"); 
+        Music Cave = Audio.LoadMusic("../../../../../Audio/(Audio/Cave.wav"); 
+        Music Chill = Audio.LoadMusic("../../../../../Audio/(Audio/Chill.wav"); 
         Music Credits = Audio.LoadMusic("../../../../../Audio/(Audio/credits music.wav"); 
-        Music REDSUN = Audio.LoadMusic("../../../../../Audio/(Audio//Armstrong/Redsun.wav"); 
+        Music Redsun = Audio.LoadMusic("../../../../../Audio/(Audio//Armstrong/Redsun.wav"); 
         Music Idiot = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Senator Armstrong - Idiot.wav"); 
         Music NanoMachines = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/_Senator - Nanomachines, Son.wav"); 
         Music Omelette = Audio.LoadMusic("../../../../../Audio/(Audio/Armstrong/Making the mother of all omelettes here Jack.wav"); 
